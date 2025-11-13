87a8e676d2dabcb3f80e079dc8f3fd69fdbdc9ca
Rafael Guzman <rega1237@gmail.com>
2025-11-13 10:56:57 -0400

    feat(config): add devtools options and env example files
    Add configuration files for Dart & Flutter DevTools and Firebase environment variables.
    Update Firebase config to use const constructors for environment variables.

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,12 +5,12 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-            apiKey: String.fromEnvironment('FIREBASE_API_KEY', defaultValue: ''),
-            authDomain: String.fromEnvironment('FIREBASE_AUTH_DOMAIN', defaultValue: ''),
-            projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: ''),
-            storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: ''),
-            messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: ''),
-            appId: String.fromEnvironment('FIREBASE_APP_ID', defaultValue: '')));
+            apiKey: const String.fromEnvironment('FIREBASE_API_KEY', defaultValue: ''),
+            authDomain: const String.fromEnvironment('FIREBASE_AUTH_DOMAIN', defaultValue: ''),
+            projectId: const String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: ''),
+            storageBucket: const String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: ''),
+            messagingSenderId: const String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: ''),
+            appId: const String.fromEnvironment('FIREBASE_APP_ID', defaultValue: '')));
   } else {
     await Firebase.initializeApp();
   }
